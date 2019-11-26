c354a8e439bb27a1e45eee5005cdaf53b34bc73b
Konstantin Kiritsenko <k@componentk.com>
2019-11-26 20:28:24 +0300

    MAGENTO2-406 fixing all product export tests
    Signed-off-by: Konstantin Kiritsenko <k@componentk.com>

--- src/Test/Integration/Model/Export/ProductTest.php
+++ src/Test/Integration/Model/Export/ProductTest.php
@@ -22,10 +22,9 @@
 namespace Shopgate\Export\Test\Integration\Model\Export;
 
 use Exception;
-use Magento\Catalog\Api\Data\ProductTierPriceInterface;
-use Magento\Catalog\Model\Product;
-use Magento\Catalog\Model\ProductFactory;
+use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Customer\Model\GroupManagement;
+use Magento\Framework\Exception\NoSuchEntityException;
 use Magento\TestFramework\ObjectManager;
 use PHPUnit\Framework\TestCase;
 use Shopgate\Export\Model\Export\Product as SubjectUnderTest;
@@ -35,79 +34,77 @@ use Shopgate_Model_Catalog_TierPrice;
  * @magentoAppIsolation enabled
  * @magentoDbIsolation  enabled
  * @magentoAppArea      frontend
+ * @magentoDataFixture  ../../../../vendor/shopgate/cart-integration-magento2-export/src/Test/Integration/fixtures/product_with_tier_pricing.php
  */
 class ProductTest extends TestCase
 {
     /** @var SubjectUnderTest */
     protected $subjectUnderTest;
-    /** @var ProductFactory */
-    protected $productFactory;
     /** * @var \Magento\Framework\App\ObjectManager */
     protected $objectManager;
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
 
     /**
      * Setup
      */
     public function setUp()
     {
-        $this->objectManager    = ObjectManager::getInstance();
-        $this->subjectUnderTest = $this->objectManager->create(SubjectUnderTest::class);
-        $this->productFactory   = $this->objectManager->create(ProductFactory::class);
+        $this->objectManager     = ObjectManager::getInstance();
+        $this->subjectUnderTest  = $this->objectManager->create(SubjectUnderTest::class);
+        $this->productRepository = $this->objectManager->create(ProductRepositoryInterface::class);
     }
 
     /**
-     * @param int   $expected
-     * @param float $salePrice
-     * @param array $tierPrices
+     * Checking both fixed and percentage tier prices
+     * Check fixture for values, they go like this:
+     * 1) 1000-8$
+     * 2) 1000-20$
+     * 3) 1000-50%
      *
-     * @dataProvider tierPriceReductionProvider
      * @throws Exception
      */
-    public function testNonRuleTierPrices($expected, $salePrice, $tierPrices): void
+    public function testNonRuleTierPrices(): void
     {
-        $product = $this->createProduct()->setFinalPrice($salePrice);
-        $this->addTierPrices($product, $tierPrices);
+        $product = $this->productRepository->get('simple-test-product');
         $this->subjectUnderTest->setItem($product)->setPrice();
         $export = $this->subjectUnderTest->getPrice()->getTierPricesGroup();
 
         /** @var Shopgate_Model_Catalog_TierPrice $group */
-        $group = array_pop($export);
-        $this->assertEquals($expected, $group->getReduction());
+        foreach ($export as $group) {
+            $this->assertContains($group->getReduction(), ['992', '980', '500']);
+        }
     }
 
     /**
-     * @param $expected
-     * @param $tierPrices
-     *
-     * @dataProvider tierPriceUidProvider
      * @throws Exception
      */
-    public function testGroupUids($expected, $tierPrices): void
+    public function testGroupUids(): void
     {
-        $product = $this->createProduct();
-        $this->addTierPrices($product, $tierPrices);
+        $product = $this->productRepository->get('simple-test-product');
         $this->subjectUnderTest->setItem($product)->setPrice();
         $export = $this->subjectUnderTest->getPrice()->getTierPricesGroup();
 
-        /** @var Shopgate_Model_Catalog_TierPrice $group */
-        $group = array_pop($export);
-        $this->assertEquals($expected, $group->getCustomerGroupUid());
+        $this->assertEquals($export[1]->getCustomerGroupUid(), (string) GroupManagement::NOT_LOGGED_IN_ID);
+        $this->assertEquals($export[2]->getCustomerGroupUid(), '1');
     }
 
     /**
-     * @param string $expectedJson
-     * @param int    $productId
+     * @param string $expectedType
+     * @param int    $sku
      *
      * @dataProvider orderInfoProvider
      * @throws Exception
      */
-    public function testOrderInfoJson($expectedJson, $productId): void
+    public function testOrderInfoJson($expectedType, $sku): void
     {
-        $product = $this->createProduct()->load($productId);
+        $product = $this->productRepository->get($sku);
         $this->subjectUnderTest->setItem($product)->setInternalOrderInfo();
         $orderInfo = $this->subjectUnderTest->getInternalOrderInfo();
         $this->assertJson($orderInfo);
-        $this->assertEquals($expectedJson, $orderInfo);
+        /** @noinspection PhpComposerExtensionStubsInspection */
+        $info = json_decode($orderInfo, true);
+        $this->assertEquals($expectedType, $info['item_type']);
     }
 
     /**
@@ -115,52 +112,20 @@ class ProductTest extends TestCase
      */
     public function testSetWeight(): void
     {
-        $product = $this->createProduct();
+        $product = $this->productRepository->get('simple-test-product');
         $this->subjectUnderTest->setItem($product)->setWeight();
-        $this->assertEquals($product->getData('weight'), $this->subjectUnderTest->getWeight());
+        $this->assertEquals(15.5, $this->subjectUnderTest->getWeight());
     }
 
     /**
      * @covers ::setAttributeGroups
+     * @throws NoSuchEntityException
      */
     public function testNotEmptyAttributeGroups(): void
     {
-        $product = $this->createProduct()->load(83);
+        $product = $this->productRepository->get('MH01');
         $this->subjectUnderTest->setItem($product)->setAttributeGroups();
         $this->assertNotEmpty($this->subjectUnderTest->getAttributeGroups());
-    }
-
-    /**
-     * Tests reductions
-     *
-     * @return array
-     */
-    public function tierPriceReductionProvider(): array
-    {
-        return [
-            '5 off'    => [
-                'expected'    => 5,
-                'final price' => 20,
-                'tier prices' => [
-                    [
-                        'group_id' => '1',
-                        'qty'      => '4',
-                        'value'    => '15',
-                    ]
-                ]
-            ],
-            '100% off' => [
-                'expected'    => 0,
-                'final price' => 20,
-                'tier prices' => [
-                    [
-                        'group_id' => '1',
-                        'qty'      => '4',
-                        'value'    => '20',
-                    ]
-                ]
-            ]
-        ];
     }
 
     /**
@@ -199,80 +164,13 @@ class ProductTest extends TestCase
      *
      * @return array
      */
-    public function orderInfoProvider()
+    public function orderInfoProvider(): array
     {
         return [
-            'simple product order info #1'     => ['{"store_view_id":"1","product_id":"1","item_type":"simple"}', 1],
-            'simple product order info #2'     => ['{"store_view_id":"1","product_id":"2","item_type":"simple"}', 2],
-            'config product order info #67'    => [
-                '{"store_view_id":"1","product_id":"67","item_type":"configurable"}',
-                67
-            ],
-            'config product order info #83'    => [
-                '{"store_view_id":"1","product_id":"83","item_type":"configurable"}',
-                83
-            ],
-            'grouped product order info #2046' => [
-                '{"store_view_id":"1","product_id":"2046","item_type":"grouped"}',
-                2046
-            ]
+            'simple product order for simple'       => ['simple', '24-MB01'],
+            'config product order for configurable' => ['configurable', 'MH01'],
+            'grouped product order for grouped'     => ['grouped', '24-WG085_Group'],
+            'grouped product order for bundled '    => ['bundle', '24-WG080']
         ];
-    }
-
-    /**
-     * Creates a simple product
-     *
-     * @return Product
-     * @throws Exception
-     */
-    private function createProduct()
-    {
-        /** @var Product $categoryFactory */
-        $product = $this->productFactory->create();
-        $product->setName('Test Product')
-                ->setTypeId('simple')
-                ->setAttributeSetId(4)
-                ->setSku('test-SKU')
-                ->setWebsiteIds([(1)])
-                ->setVisibility(4)
-                ->setPrice(50)
-                ->setWeight(23.5)
-                ->setData('image', '/testimg/test.jpg')
-                ->setData('small_image', '/testimg/test.jpg')
-                ->setData('thumbnail', '/testimg/test.jpg')
-                ->setData(
-                    'stock_data',
-                    [
-                        'use_config_manage_stock' => 0,
-                        'manage_stock'            => 1,
-                        'min_sale_qty'            => 1,
-                        'max_sale_qty'            => 2,
-                        'is_in_stock'             => 1,
-                        'qty'                     => 100
-                    ]
-                );
-
-        return $product;
-    }
-
-    /**
-     * Helps add tier prices to the product
-     *
-     * @param Product $product
-     * @param array   $tierPrices
-     */
-    private function addTierPrices(Product $product, array $tierPrices): void
-    {
-        $nodes = [];
-        foreach ($tierPrices as $tierPrice) {
-            /** @var ProductTierPriceInterface $tierPriceNode */
-            $tierPriceNode = $this->objectManager->create(ProductTierPriceInterface::class);
-            $nodes[]       = $tierPriceNode
-                ->setCustomerGroupId($tierPrice['group_id'])
-                ->setQty($tierPrice['qty'])
-                ->setValue($tierPrice['value']);
-        }
-
-        $product->setTierPrices($nodes);
     }
 }
